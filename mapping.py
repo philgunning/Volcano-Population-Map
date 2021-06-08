57275d05ee61f741795a312daa0dcd0feb8590df
Phil Gunning <55432795+philgunning@users.noreply.github.com>
2021-06-08 23:26:18 +0100

    Add files via upload

---
+++ mapping.py
@@ -0,0 +1,40 @@
+import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+name = list(data["NAME"])
+ 
+html = """<br>Volcano Name: <a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a></br>
+    <br>Height: %s m</br>
+    """
+
+def color_producer(elevation):
+    if elevation < 1500:
+        return "green"
+    elif 1500 <= elevation < 3000:
+        return "orange"
+    else:
+        return "red"
+
+map = folium.Map(location=[38.58, -99.09], zoom_start=4, tiles="Stamen Terrain")
+
+fgp = folium.FeatureGroup(name = "Populations")
+fgp.add_child(folium.GeoJson(data = (open("world.json", "r", encoding="utf-8-sig").read()),
+    style_function=lambda x: {"fillColor":"green" if x['properties']['POP2005'] < 10000000
+        else "orange" if 10000000 <= x['properties']['POP2005'] < 20000000 else "red" }))
+
+fgv = folium.FeatureGroup(name = "Volcanoes")
+for lt, ln, el, name in zip(lat, lon, elev, name):
+    iframe = folium.IFrame(html=html % (name, name, int(el)), width=200, height=100)
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=folium.Popup(iframe),
+        fill_color=color_producer(el), color="grey", fill_opacity=0.7 ))
+
+map.add_child(fgp)
+map.add_child(fgv)
+map.add_child(folium.LayerControl())
+
+map.save("map.html")
+
